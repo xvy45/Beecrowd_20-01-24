f23690eecf30112ddcd09376902452be510807cc
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-01-22 21:18:43 +0600

    Add files via upload

---
+++ C#/1001/1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Say_Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, X;
+
+           A =  Convert.ToInt32(Console.ReadLine()); 
+           B =  Convert.ToInt32(Console.ReadLine());
+           X = A + B;
+           Console.WriteLine("X = " + X);
+        }
+    }
+}
